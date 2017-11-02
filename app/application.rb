693c59e79f21bd7208af82380d581a576a93af34
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-02 14:23:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
       time=Time.now
-    resp.write "#{time}"
+      if time.to_s.split(" ")[1].split(":")[0].to_i < 12
+    resp.write "Good morning it is now #{time.to_s.split(" ")[1]} in London"
     resp.finish
   end
 
