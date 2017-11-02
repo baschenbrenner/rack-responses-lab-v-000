66c9bb1908fb11e59cdae8f97afa861c19aea4c3
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-02 14:25:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,9 @@ class Application
     resp = Rack::Response.new
       time=Time.now
       if time.to_s.split(" ")[1].split(":")[0].to_i < 12
-        resp.write "Good morning! It is now #{time.to_s.split(" ")[1]} in London"
+        resp.write "Good morning! It is now #{time.to_s.split(" ")[1]} in London. A beautiful Morning"
       elsif time.to_s.split(" ")[1].split(":")[0].to_i > 11
-        resp.write "Good afternoon! It is now #{time.to_s.split(" ")[1]} in London"
+        resp.write "Good afternoon! It is now #{time.to_s.split(" ")[1]} in London. A beautiful Afternoon"
       end
   
     resp.finish
